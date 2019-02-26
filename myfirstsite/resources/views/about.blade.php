ccd30e78b6fd4f34771477cf2bd26ae4a88ebcb5
Johan Nordström <johan@digitalvillage.se>
2019-02-26 13:33:56 +0100

    First playtime with Blade templates.

--- myfirstsite/resources/views/about.blade.php
+++ myfirstsite/resources/views/about.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts/app')
 
 @section('content')
-	<h1>Welcome to My First Site!</h1>
+	<h1>About Me</h1>
 @endsection
--- myfirstsite/resources/views/contact.blade.php
+++ myfirstsite/resources/views/contact.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts/app')
 
 @section('content')
-	<h1>Welcome to My First Site!</h1>
+	<h1>Contact Me</h1>
 @endsection

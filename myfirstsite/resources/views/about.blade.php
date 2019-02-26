eff2267d0a9bddd8369a61cc7c1644cfdcffc446
Johan Nordström <johan@digitalvillage.se>
2019-02-26 14:06:44 +0100

    Extending of Blade templates, and also some including.

--- myfirstsite/resources/views/about.blade.php
+++ myfirstsite/resources/views/about.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts/app')
 
+@section('title', 'About Me')
+
 @section('content')
 	<h1>About Me</h1>
 @endsection
--- myfirstsite/resources/views/contact.blade.php
+++ myfirstsite/resources/views/contact.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts/app')
 
+@section('title', 'Contact Me')
+
 @section('content')
 	<h1>Contact Me</h1>
 @endsection

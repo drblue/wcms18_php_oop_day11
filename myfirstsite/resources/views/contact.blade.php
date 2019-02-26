c64ce4b15fe34ee28656d49a21d5f0289729c767
Johan Nordström <johan@digitalvillage.se>
2019-02-26 14:12:11 +0100

    Some more content and an example of a different yield.

--- myfirstsite/resources/views/contact.blade.php
+++ myfirstsite/resources/views/contact.blade.php
@@ -4,4 +4,12 @@
 
 @section('content')
 	<h1>Contact Me</h1>
+
+	<p>Lorem ipsum dolor amet squid sed culpa occaecat crucifix kinfolk VHS. Chartreuse hammock single-origin coffee, venmo pickled cliche kickstarter shabby chic affogato fashion axe. Aliqua copper mug et leggings cornhole pork belly, occaecat listicle incididunt poutine deep v before they sold out. Eiusmod in chartreuse kale chips 90's officia exercitation sustainable tempor subway tile. 3 wolf moon everyday carry taxidermy cupidatat, cray vexillologist umami knausgaard ullamco. Cold-pressed mixtape coloring book, squid kogi synth lorem air plant 90's aliqua. Ex 3 wolf moon skateboard anim slow-carb glossier.</p>
+
+	<p>Pickled stumptown yuccie meditation, lorem church-key cliche proident retro keffiyeh chia taxidermy man bun vaporware. Subway tile four dollar toast labore nisi, pork belly ut lorem kale chips. Pour-over irony twee put a bird on it taiyaki butcher do intelligentsia pop-up dreamcatcher occaecat. Direct trade listicle aliqua banh mi in literally lo-fi mollit cold-pressed vaporware selvage bicycle rights before they sold out commodo stumptown. Vexillologist waistcoat shoreditch kitsch authentic, asymmetrical listicle. Tattooed in ut id street art unicorn photo booth sriracha quis godard. Austin photo booth chicharrones enim distillery semiotics.</p>
+@endsection
+
+@section('sidebar')
+	I AM A SIDEBAR!
 @endsection
